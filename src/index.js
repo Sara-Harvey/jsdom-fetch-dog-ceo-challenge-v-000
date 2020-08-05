2aaf23bb1a8a0974947f0f638f14ba2ce1413ace
Sara Harvey <saraharveynyc@gmail.com>
2020-08-05 16:58:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 console.log('%c HI', 'color: firebrick')
 
 let breeds = []
+
+function fetchDogPics() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => {
+      for (const image in json.message) {
+        renderDogPics(json.message[image])
+      }
+    })
+}
