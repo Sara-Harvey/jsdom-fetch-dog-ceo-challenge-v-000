89af2482b6e9b602a1ff807724d2121f6ed9194e
Sara Harvey <saraharveynyc@gmail.com>
2020-08-05 17:02:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,3 +21,18 @@ function renderDogPics(dogPic) {
   image.src = dogPic
   main.appendChild(image)
 }
+
+function fetchDogBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(json => {
+      // the return value is an Array containing all of the keys at the top level of the Object
+      breeds = Object.keys(json.message)
+      // display all dog breeds on page
+      renderDogBreeds(breeds)
+      // filter breeds that start with a particular letter
+      addBreedSelectListener()
+    })
+}
